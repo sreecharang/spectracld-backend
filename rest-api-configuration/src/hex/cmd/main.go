a1e5281a95e7778744e9b0f695c3cfd844c0cca1
Sreecharan Gaddam <charan.24119@gmail.com>
2023-11-23 12:05:57 +0000

    added methods declaration.

--- rest-api-configuration/src/hex/cmd/main.go
+++ rest-api-configuration/src/hex/cmd/main.go
@@ -1,8 +1,25 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"restapi/v2/internal/adapters/core/arithmetic"
+	// "restapi/v2/internal/ports"
+)
 
 func main() {
-	fmt.Println("vim-go")
+
+	// ports 
+	// var core ports.ArithmeticPort
+
+	// core = arithmetic.NewAdapter()
+
+	arithAdapter := arithmetic.NewAdapter()
+	result, err := arithAdapter.Addition(1, 3)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(result)
 }
 
