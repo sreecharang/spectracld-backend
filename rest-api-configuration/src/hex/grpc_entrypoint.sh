b1e356fd67975612f98462d4ad36f31832181492
Sreecharan Gaddam <charan.24119@gmail.com>
2023-11-26 19:15:18 +0000

    Changes on docker compose file.

--- rest-api-configuration/src/hex/grpc_entrypoint.sh
+++ rest-api-configuration/src/hex/grpc_entrypoint.sh
@@ -4,14 +4,14 @@ set -e
 COMMAND=$@
 
 echo 'Waiting for database to be available...'
-maxTries=10 
+maxTries=20
 while [ "$maxTries" -gt 0 ] && ! mysql -h "$MYSQL_HOST" -P "$MYSQL_PORT" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DB" -e 'SHOW TABLES'; do
     maxTries=$((maxTries-=1)) 
     sleep 3
 done
 echo 
 if [ "$maxTries" -le 0 ]; then
-    echo >&2 'error: unable to contact database after 30 seconds(10 tries)'
+    echo >&2 'error: unable to contact database after 60 seconds(20 tries)'
     exit 1
 fi
 
